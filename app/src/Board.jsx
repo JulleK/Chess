d54bfd638fdc216ea99b28d819a90f389ad95cbe
Julek <julek.krzeszycha@gmail.com>
2024-05-01 17:43:10 +0200

    add orientation property

--- app/src/Board.jsx
+++ app/src/Board.jsx
@@ -1,9 +1,13 @@
 import Chessboard from "chessboardjsx";
-export default function Board({ position = "start", onDrop }) {
+export default function Board({ position = "start", onDrop, orientation }) {
   return (
     <div>
       <div className="border-2 border-black">
-        <Chessboard position={position} onDrop={onDrop} />
+        <Chessboard
+          position={position}
+          onDrop={onDrop}
+          orientation={orientation}
+        />
       </div>
     </div>
   );
