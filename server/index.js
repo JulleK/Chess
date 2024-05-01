4a4e7fcba1f27c26ffcbc98a8cbf48ba4d778bb9
Julek <julek.krzeszycha@gmail.com>
2024-05-01 17:45:09 +0200

    finish rooms and add color restriction

--- server/index.js
+++ server/index.js
@@ -2,8 +2,6 @@ import { createServer } from "http";
 import { Server } from "socket.io";
 
 import { Chess } from "chess.js";
-import { captureRejectionSymbol } from "events";
-const chess = new Chess();
 
 const server = createServer();
 const io = new Server(server, {
@@ -13,36 +11,40 @@ const io = new Server(server, {
 });
 
 const rooms = [
-  { white: "", black: "" },
-  { white: "", black: "" },
-  { white: "", black: "" },
-  { white: "", black: "" },
-  { white: "", black: "" },
+  { white: "", black: "", chess: null },
+  { white: "", black: "", chess: null },
+  { white: "", black: "", chess: null },
+  { white: "", black: "", chess: null },
+  { white: "", black: "", chess: null },
 ];
 
 function addPlayerToRoom(playerID) {
-  for (let i = 0; i < rooms.length; i++) {
-    if (!rooms[i].white) {
-      rooms[i].white = playerID;
-      console.log(rooms);
-      return `room${i}`;
-    } else if (!rooms[i].black) {
-      rooms[i].black = playerID;
-      console.log(rooms);
-      return `room${i}`;
+  let color;
+  for (let room = 0; room < rooms.length; room++) {
+    if (!rooms[room].white) {
+      rooms[room].white = playerID;
+      rooms[room].chess = new Chess();
+      console.dir(rooms, { depth: 1 });
+
+      return { room, color: "white" };
+    } else if (!rooms[room].black) {
+      rooms[room].black = playerID;
+      console.dir(rooms, { depth: 1 });
+
+      return { room, color: "black" };
     }
   }
 }
 
-function removePlayerFromRoom(socket) {
+function removePlayerFromRoom(playerID) {
   for (let i = 0; i < rooms.length; i++) {
-    if (rooms[i].white === socket.id) {
+    if (rooms[i].white === playerID) {
       rooms[i].white = "";
-      console.log(rooms);
+      console.dir(rooms, { depth: 1 });
       break;
-    } else if (rooms[i].black === socket.id) {
+    } else if (rooms[i].black === playerID) {
       rooms[i].black = "";
-      console.log(rooms);
+      console.dir(rooms, { depth: 1 });
       break;
     }
   }
@@ -52,31 +54,36 @@ io.on("connection", (socket) => {
   console.log("a player connected");
 
   const playerID = socket.id;
-  addPlayerToRoom(playerID);
+  const { room, color } = addPlayerToRoom(playerID);
+  const chess = rooms[room].chess;
+
+  socket.join(room);
 
   socket.on("startingPosition", () => {
-    socket.emit("currentPosition", chess.fen());
+    io.to(room).emit("currentPosition", chess.fen());
+    socket.emit("color", color);
   });
 
   socket.on("validate", (move) => {
     try {
+      if (chess._turn !== color[0]) throw Error("wrong color");
+      console.log(chess._turn);
       chess.move(move);
     } catch (err) {
       console.log("illegal move");
     }
-    socket.emit("currentPosition", chess.fen());
-    socket.broadcast.emit("currentPosition", chess.fen());
+    io.to(room).emit("currentPosition", chess.fen());
   });
 
   socket.on("resetGame", () => {
     chess.reset();
-    socket.emit("currentPosition", chess.fen());
-    socket.broadcast.emit("currentPosition", chess.fen());
+    io.to(room).emit("currentPosition", chess.fen());
   });
 
   socket.on("disconnect", () => {
     console.log("user disconnected");
     removePlayerFromRoom(playerID);
+    socket.leave(room);
   });
 });
 
