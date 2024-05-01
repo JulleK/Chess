3c62d1a186f389fff34839aaaf08a480c713416a
Julek <julek.krzeszycha@gmail.com>
2024-05-01 15:58:37 +0200

    move join game code into App.jsx

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,10 +1,22 @@
+import { useState } from "react";
 import Game from "./Game";
 
 export default function App() {
+  const [gameStarted, setGameStarted] = useState(false);
+
+  const joinGame = () => {
+    setGameStarted(true);
+  };
+
   return (
     <div className="h-screen w-screen bg-slate-800">
       <div className="flex h-full items-center justify-center">
-        <Game />
+        {!gameStarted && (
+          <button onClick={joinGame} className="border-2 p-10 text-white">
+            Join Game
+          </button>
+        )}
+        {gameStarted && <Game />}
       </div>
     </div>
   );
--- app/src/Game.jsx
+++ app/src/Game.jsx
@@ -1,79 +1,53 @@
 import Board from "./Board";
 import { useState, useEffect } from "react";
 import { io } from "socket.io-client";
+const socket = io("ws://localhost:3000");
 
 export default function Game() {
-  const [gameStarted, setGameStarted] = useState(false);
   const [currentPosition, setCurrentPosition] = useState(
     "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
   );
-  const [socket, setSocket] = useState(null);
 
   useEffect(() => {
-    if (socket) {
-      socket.on("connect", () => {
-        console.log("Server connection estabilished");
-      });
+    socket.on("connect", () => {
+      console.log("Server connection estabilished");
+    });
 
-      socket.on("currentPosition", (position) => {
-        console.log(position);
-        setCurrentPosition(position);
-      });
-    }
-  }, [socket]);
+    socket.emit("startingPosition");
 
-  const joinGame = () => {
-    setGameStarted(true);
-    const newSocket = io("ws://localhost:3000");
-    setSocket(newSocket);
-  };
+    socket.on("currentPosition", (position) => {
+      setCurrentPosition(position);
+    });
+  });
 
-  const onDrop = (move) => {
+  const onDrop = ({ piece, sourceSquare, targetSquare, promotion = "" }) => {
     if (
-      (move.targetSquare[1] === "8" || move.targetSquare[1] === "1") &&
-      move.piece[1] === "P"
+      (targetSquare[1] === "8" || targetSquare[1] === "1") &&
+      piece[1] === "P"
     )
-      move.promotion = "Q"; // always promote to queen
-    validateMove(move);
-  };
-
-  const validateMove = ({
-    piece,
-    sourceSquare,
-    targetSquare,
-    promotion = "",
-  }) => {
+      promotion = "Q"; // always promote to queen
     let move = `${piece[1]}${sourceSquare}-${targetSquare}${promotion}`;
     socket.emit("validate", move);
   };
 
   const resetGame = () => {
     socket.emit("resetGame");
-    console.log("game resetted");
   };
 
   return (
     <>
-      {!gameStarted && (
-        <button onClick={joinGame} className="border-2 p-10 text-white">
-          Join Game
-        </button>
-      )}
-
-      {gameStarted && (
-        <div>
-          <div className="border-2 border-black">
-            <Board position={currentPosition} onDrop={onDrop} />
-          </div>
-
-          <button
-            className="mt-5 rounded-sm border-2 p-1 text-white"
-            onClick={resetGame}
-          >
-            Reset Game
-          </button>
+      <div>
+        <div className="border-2 border-black">
+          <Board position={currentPosition} onDrop={onDrop} />
         </div>
-      )}
+
+        <button
+          className="mt-5 rounded-sm border-2 p-1 text-white"
+          onClick={resetGame}
+        >
+          Reset Game
+        </button>
+      </div>
     </>
   );
 }
