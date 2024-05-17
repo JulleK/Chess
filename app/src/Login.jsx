bceb9c1866c95d9c6eeceafc83bedb5d51d7bd89
Julek <julek.krzeszycha@gmail.com>
2024-05-17 20:50:29 +0200

    signup/login form, style only

--- app/src/Login.jsx
+++ app/src/Login.jsx
@@ -1,3 +1,15 @@
 export default function Login() {
-  return <h1>Login</h1>;
+  return (
+    <form>
+      <h3 className="text-center">Login</h3>
+      <input type="text" placeholder="username" name="username" id="username" />
+      <input
+        type="password"
+        placeholder="password"
+        name="password"
+        id="password"
+      />
+      <button className="register">Login</button>
+    </form>
+  );
 }
--- app/src/Signup.jsx
+++ app/src/Signup.jsx
@@ -1,3 +1,16 @@
 export default function Signup() {
-  return <h1>Signup</h1>;
+  return (
+    <form>
+      <h3 className="text-center">Signup</h3>
+      <input type="email" placeholder="email" name="email" id="email" />
+      <input type="text" placeholder="username" name="username" id="username" />
+      <input
+        type="password"
+        placeholder="password"
+        name="password"
+        id="password"
+      />
+      <button className="register">Register</button>
+    </form>
+  );
 }
