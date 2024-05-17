6ba4fae4d8ecdbb7a3b28d7281eda411d5cc5f63
Julek <julek.krzeszycha@gmail.com>
2024-05-17 22:15:43 +0200

    make controlled forms

--- app/src/Login.jsx
+++ app/src/Login.jsx
@@ -1,15 +1,33 @@
+import { useState } from "react";
 export default function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = () => {
+    // todo
+  };
   return (
     <form>
       <h3 className="text-center">Login</h3>
-      <input type="text" placeholder="username" name="username" id="username" />
+      <input
+        type="text"
+        placeholder="username"
+        name="username"
+        id="username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
       <input
         type="password"
         placeholder="password"
         name="password"
         id="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
       />
-      <button className="register">Login</button>
+      <button className="register" onClick={handleSubmit}>
+        Login
+      </button>
     </form>
   );
 }
--- app/src/Signup.jsx
+++ app/src/Signup.jsx
@@ -1,16 +1,43 @@
+import { useState } from "react";
 export default function Signup() {
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = () => {
+    // todo
+  };
+
   return (
     <form>
       <h3 className="text-center">Signup</h3>
-      <input type="email" placeholder="email" name="email" id="email" />
-      <input type="text" placeholder="username" name="username" id="username" />
+      <input
+        type="email"
+        placeholder="email"
+        name="email"
+        id="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="username"
+        name="username"
+        id="username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
       <input
         type="password"
         placeholder="password"
         name="password"
         id="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
       />
-      <button className="register">Register</button>
+      <button className="register" onClick={handleSubmit}>
+        Register
+      </button>
     </form>
   );
 }
